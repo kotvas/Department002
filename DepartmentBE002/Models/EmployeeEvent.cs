34e1e06df102b19c15b0c607c066d1bd39c48108
kotvas <kotv17@gmail.com>
2016-06-17 16:53:55 +0300

    EmployeeEvent and EmployeeType classes were added

---
+++ DepartmentBE002/Models/EmployeeEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentBE002.Models
+{
+    public class EmployeeEvent
+    {
+        public Guid Id { get; set; }
+        public EventType Type { get; set; }
+        public Employee ResponsibleEmployee { get; set; }
+        public List<Employee> InvitedEmployees { get; set; }
+    }
+}
